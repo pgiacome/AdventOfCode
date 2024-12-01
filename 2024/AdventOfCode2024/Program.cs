ca977c3dc7dd1ae0573ff3055e72fad2760f51e6
pgiacomelli <pgiacome@gmail.com>
2024-12-01 13:00:43 +0100

    d

--- 2024/AdventOfCode2024/Program.cs
+++ 2024/AdventOfCode2024/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Day01();
+    }
+
+    private static void Day01()
+    {
+
+        InputFolder = Environment.CurrentDirectory;
+
+        folderSeparator = System.IO.Path.DirectorySeparatorChar.ToString();
+
+        CurrentDay = DownloadFolder +  folderSeparator + "Day01" + folderSeparator + "input.txt";
+
+        var lines = System.IO.File.ReadAllLines(filePath);
+
+    }
+}
